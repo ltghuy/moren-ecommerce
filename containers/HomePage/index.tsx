65129d8b47ec30e8c8dc316ec84684b810b09ba9
ltghuy <letrangiahuy0507@gmail.com>
2022-07-16 11:12:17 +0700

    Fix tailwind

--- containers/HomePage/index.tsx
+++ containers/HomePage/index.tsx
@@ -48,8 +48,8 @@ const HomePage = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fringilla quis ipsum enim viverra. Enim in morbi tincidunt ante luctus tincidunt integer.
           </p>
         </section>
-        <section className={`${styles.banner} flex justify-between w-full mb-10 lg:mb-20 relative`}>
-          <div className='flex-[5]'>
+        <section className={`${styles.banner} flex justify-between w-full relative`}>
+          <div className={styles.col1}>
             <a href="/shop" className={`${styles.banner__link} group relative`}>
               <img src="/images/col1.jpg" alt="col-1" />
               <div className='absolute inset-0 bg-overlay hidden justify-center items-center text-2xl montserrat group-hover:flex'>
@@ -60,7 +60,7 @@ const HomePage = () => {
               </div>
             </a>
           </div>
-          <div className='flex-[4] ml-12 flex flex-col'>
+          <div className={`${styles.col2} flex flex-col`}>
             <a href="/shop" className={`${styles.banner__link} group relative`}>
               <img src="/images/col2.jpg" alt="col-2" />
               <div className='absolute inset-0 bg-overlay hidden justify-center items-center text-2xl montserrat group-hover:flex'>
@@ -70,7 +70,9 @@ const HomePage = () => {
                   </span>
               </div>
             </a>
-            <a href="/shop" className={`${styles.banner__link} group relative mt-12`}>
+            <a href="/shop" 
+               className={`${styles.banner__link} group relative`}
+               style={{marginTop: '3rem'}}>
               <img src="/images/col3.jpg" alt="col-3" />
               <div className='absolute inset-0 bg-overlay hidden justify-center items-center text-2xl montserrat group-hover:flex'>
                   <span className='text-white mr-3'>VIEW ALL</span>
@@ -96,9 +98,9 @@ const HomePage = () => {
               )
             }
           </div>
-          <div className='w-full text-center mb-12 xl:mb-20'>
+          <div className={`${styles.viewBtn} w-full`}>
             <Link href="/shop">
-              <a className='w-32 h-10 leading-10 montserrat inline-block text-center border border-black hover:text-white hover:bg-primary hover:border-primary'>
+              <a className='montserrat border border-black hover:text-white hover:bg-primary hover:border-primary'>
                 VIEW ALL
               </a>
             </Link>
@@ -118,25 +120,25 @@ const HomePage = () => {
               )
             }
           </div>
-          <div className='w-full text-center mb-12 xl:mb-20'>
+          <div className={`${styles.viewBtn} w-full`}>
             <Link href="/shop">
-              <a className='w-32 h-10 leading-10 montserrat inline-block text-center border border-black hover:text-white hover:bg-primary hover:border-primary'>
+              <a className='montserrat border border-black hover:text-white hover:bg-primary hover:border-primary'>
                 VIEW ALL
               </a>
             </Link>
           </div>
         </section>
         <section className={styles.subscribe}>
-            <h1 className='text-center font-semibold montserrat text-3xl lg:text-5xl'>NEWSLETTERS</h1>
-            <p className='text-base lg:text-xl lg:leading-9 text-center pt-5'>
+            <h1 className={`${styles.title} font-semibold montserrat`}>NEWSLETTERS</h1>
+            <p className={`${styles.para} text-base lg:text-xl pt-5`}>
             Subscribe our newsletter to get notify about discount and latest update. Don’t worry, we not spam!
             </p>
-            <div className='w-full lg:w-98 h-14 lg:h-16 flex justify-between border-b border-gray-300 mx-auto mt-5 lg:mt-10'>
+            <div className={`${styles.form} flex justify-between mx-auto`}>
               <input
               type="text"
               placeholder='Enter your email here'
-              className='outline-0 border-0 h-full flex-1 pr-4 text-md lg:text-xl leading-9 text-gray-400'/>
-              <button className='w-8 h-full flex-shrink-0 text-3xl font-normal hover:text-orange-800'>
+              className='h-full flex-1 pr-4 text-md lg:text-xl'/>
+              <button className='h-full'>
                 <FontAwesomeIcon icon={faArrowRightLong} />
               </button>
             </div>
